07138f17edf72c85c4f0617ad5cd828105de8eca
Eirik Granvin Bakke <eirikgba@stud.ntnu.no>
2021-09-17 17:52:16 +0200

    flyttet files

--- createpasswd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-filename="xato-net-10-million-passwords-1000.txt"
-for word1 in $(cat $filename);
-do
-        for word2 in $(cat $filename);
-                do
-                        echo $word1$word2
-                done
-done
